06b17c9eb3d1a42badb42e5cf57b283a3519e377
matildawedin <matti_wedin@hotmail.com>
2020-09-26 12:38:13 +0200

    .

---
+++ src/application/UrlLogin.java
@@ -0,0 +1,9 @@
+package application;
+
+public class UrlLogin {
+	
+	protected static String url ="jdbc:sqlserver://cloud0server.database.windows.net:1433;database=azureDatabas";
+	protected static String user = "matilda";
+	protected static String password = "SMguld2007";
+
+}
