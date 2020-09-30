22f351ecb8bb76f4e1d4a5c3bc1083993f0ebe9d
LeoRasmusson <56584677+LeoRasmusson@users.noreply.github.com>
2020-09-30 12:51:23 +0200

    More imports goneso

--- src/application/DbConnection.java
+++ src/application/DbConnection.java
@@ -2,8 +2,6 @@ package application;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DbConnection extends UrlLogin {
